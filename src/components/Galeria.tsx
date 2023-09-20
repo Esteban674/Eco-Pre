91368dc796294b1b1bd8de1b33be0efb55ac8d28
Esteban Perea <estebanperea@gmail.com>
2023-09-20 15:16:57 -0300

    galeria actualizada

--- src/components/Galeria.tsx
+++ src/components/Galeria.tsx
@@ -3,70 +3,114 @@ import { Galleria } from 'primereact/galleria';
 
 interface Image {
   itemImageSrc: string;
-  thumbnailImageSrc: string;
   alt: string;
   title: string;
 }
 
-const imagenes: Image[] = [
+const imagenes1: Image[] = [
   {
-    itemImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1690755868/Web-Ecologica/Construcciones/Screenshot_20230526-110307_2_riniaj.png',
-    thumbnailImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1690755910/Web-Ecologica/Construcciones/Mini/Screenshot_20230526-110307_2_fyzxvr.png',
+    itemImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1695227440/Web-Ecologica/Construcciones/Foto_5_modificada_2x_1_a9utbh.jpg',
     alt: 'Description for Image 1',
     title: 'Title 1'
   },
   {
-    itemImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1690755017/Web-Ecologica/Construcciones/Screenshot_20230526-105820_2_lgey5x.png',
-    thumbnailImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1690755340/Web-Ecologica/Construcciones/Mini/Screenshot_20230526-105820_2_zuslzq.png',
+    itemImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1695227476/Web-Ecologica/Construcciones/Vivienda_peyid8.jpg',
     alt: 'Description for Image 2',
     title: 'Title 2'
   },
   {
-    itemImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1690755026/Web-Ecologica/Construcciones/Screenshot_20230526-110245_2_poap1n.png',
-    thumbnailImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1690755339/Web-Ecologica/Construcciones/Mini/Screenshot_20230526-110245_2_rpdxnj.png',
+    itemImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1695227464/Web-Ecologica/Construcciones/Foto_7_modificada_2x_1_ut5fox.jpg',
     alt: 'Description for Image 2',
     title: 'Title 2'
   },
   {
-    itemImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1690755021/Web-Ecologica/Construcciones/Screenshot_20230526-105849_2_gfovkg.png',
-    thumbnailImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1690755339/Web-Ecologica/Construcciones/Mini/Screenshot_20230526-105849_2_v3i5rr.png',
+    itemImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1695227425/Web-Ecologica/Construcciones/Foto_4_Modificada_1_vffc71.jpg',
     alt: 'Description for Image 2',
     title: 'Title 2'
   },
   {
-    itemImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1690755020/Web-Ecologica/Construcciones/Screenshot_20230526-112103_2_i0sysp.png',
-    thumbnailImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1690755340/Web-Ecologica/Construcciones/Mini/Screenshot_20230526-112103_2_lnzvmo.png',
+    itemImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1695227394/Web-Ecologica/Construcciones/Foto_2_MODIFICADA_1_flwrlc.jpg',
     alt: 'Description for Image 2',
     title: 'Title 2'
   },
   {
-    itemImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1690755018/Web-Ecologica/Construcciones/Screenshot_20230526-110357_2_xlv5cm.png',
-    thumbnailImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1690755340/Web-Ecologica/Construcciones/Mini/Screenshot_20230526-110357_2_aqs7jv.png',
+    itemImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1695227383/Web-Ecologica/Construcciones/Foto_1_MODIFICADA_Con_libustro_1_to4vma.jpg',
     alt: 'Description for Image 2',
     title: 'Title 2'
   },
   {
-    itemImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1690755018/Web-Ecologica/Construcciones/Screenshot_20230526-110120_2_oi99cn.png',
-    thumbnailImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1690755339/Web-Ecologica/Construcciones/Mini/Screenshot_20230526-110120_2_fncqb8.png',
+    itemImageSrc: 'https://res.cloudinary.com/duqilis2i/image/upload/v1695125800/Eco-Pre/imagenes%20livianas/img5_dxhp5q.jpg',
     alt: 'Description for Image 2',
     title: 'Title 2'
   },
   {
-    itemImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1690755019/Web-Ecologica/Construcciones/Screenshot_20230526-112131_2_xef69q.png',
-    thumbnailImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1690755339/Web-Ecologica/Construcciones/Mini/Screenshot_20230526-112131_2_jtrb3w.png',
+    itemImageSrc: 'https://res.cloudinary.com/duqilis2i/image/upload/v1695125796/Eco-Pre/imagenes%20livianas/img14_me4j9q.jpg',
+    alt: 'Description for Image 2',
+    title: 'Title 2'
+  },
+];
+
+const imagenes2: Image[] = [
+  {
+    itemImageSrc: 'https://res.cloudinary.com/duqilis2i/image/upload/v1695125796/Eco-Pre/imagenes%20livianas/img19_gujwap.jpg',
+    alt: 'Description for Image 1',
+    title: 'Title 1'
+  },
+  {
+    itemImageSrc: 'https://res.cloudinary.com/duqilis2i/image/upload/v1695125794/Eco-Pre/imagenes%20livianas/img10_oopdye.jpg',
     alt: 'Description for Image 2',
     title: 'Title 2'
   },
   {
-    itemImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1690755020/Web-Ecologica/Construcciones/Screenshot_20230526-110321_2_fjmpw8.png',
-    thumbnailImageSrc: 'https://res.cloudinary.com/dmehzrcf1/image/upload/v1690755340/Web-Ecologica/Construcciones/Mini/Screenshot_20230526-110321_2_d13xqb.png',
+    itemImageSrc: 'https://res.cloudinary.com/duqilis2i/image/upload/v1695125801/Eco-Pre/imagenes%20livianas/img7_bstx7i.jpg',
+    alt: 'Description for Image 2',
+    title: 'Title 2'
+  },
+  {
+    itemImageSrc: 'https://res.cloudinary.com/duqilis2i/image/upload/v1695125800/Eco-Pre/imagenes%20livianas/img34_axgvsz.jpg',
+    alt: 'Description for Image 2',
+    title: 'Title 2'
+  },
+  {
+    itemImageSrc: 'https://res.cloudinary.com/duqilis2i/image/upload/v1695125798/Eco-Pre/imagenes%20livianas/img24_qpkuq8.jpg',
+    alt: 'Description for Image 2',
+    title: 'Title 2'
+  },
+  {
+    itemImageSrc: 'https://res.cloudinary.com/duqilis2i/image/upload/v1695125798/Eco-Pre/imagenes%20livianas/img27_lwhqln.jpg',
+    alt: 'Description for Image 2',
+    title: 'Title 2'
+  },
+  {
+    itemImageSrc: 'https://res.cloudinary.com/duqilis2i/image/upload/v1695125797/Eco-Pre/imagenes%20livianas/img21_zl9xl0.jpg',
+    alt: 'Description for Image 2',
+    title: 'Title 2'
+  },
+  {
+    itemImageSrc: 'https://res.cloudinary.com/duqilis2i/image/upload/v1695125797/Eco-Pre/imagenes%20livianas/img22_oia88t.jpg',
+    alt: 'Description for Image 2',
+    title: 'Title 2'
+  },
+  {
+    itemImageSrc: 'https://res.cloudinary.com/duqilis2i/image/upload/v1695125796/Eco-Pre/imagenes%20livianas/img16_xxtho6.jpg',
+    alt: 'Description for Image 2',
+    title: 'Title 2'
+  },
+  {
+    itemImageSrc: 'https://res.cloudinary.com/duqilis2i/image/upload/v1695125801/Eco-Pre/imagenes%20livianas/img8_dqwjpo.jpg',
+    alt: 'Description for Image 2',
+    title: 'Title 2'
+  },
+  {
+    itemImageSrc: 'https://res.cloudinary.com/duqilis2i/image/upload/v1695125799/Eco-Pre/imagenes%20livianas/img3_hhiadp.jpg',
     alt: 'Description for Image 2',
     title: 'Title 2'
   },
 ];
 
 export const Galeria = () => {
-  const [images, setImages] = useState<Image[] | null>(null);
+  const [images1, setImages1] = useState<Image[] | null>(null);
+  const [images2, setImages2] = useState<Image[] | null>(null);
 
 
   const responsiveOptions = [
@@ -85,31 +129,48 @@ export const Galeria = () => {
   ];
 
   useEffect(() => {
-    setImages(imagenes);
+    setImages1(imagenes1);
+    setImages2(imagenes2);
   }, []);
 
   const itemTemplate = (item: Image) => {
-    return <img src={item.itemImageSrc} alt={item.alt} style={{ width: '100%', display: 'block' }} />;
+    return <img src={item.itemImageSrc} alt={item.alt} style={{ width: '100%', display: 'block' }} />
   }
 
   return (
     <>
-    <div className="container-gallery card flex justify-content-center"> 
-      {images && (
-        <Galleria
-          className="gallery"
-          value={images}
-          responsiveOptions={responsiveOptions}
-          numVisible={4}
-          circular
-          style={{ maxWidth: '760px', minWidth: '280px' }}
-          showItemNavigators
-          item={itemTemplate}
-      
-          // thumbnailsPosition={"right"}
-        />
-      )}
-    </div>
+    <div className="container-gallery card flex flex-column justify-content-center align-items-center">
+    <h2 className="titulo-galeria text-center m-0 pt-3">Obras Terminadas</h2>
+        {images1 && (
+          <Galleria
+            className="gallery"
+            value={images1}
+            responsiveOptions={responsiveOptions}
+            numVisible={4}
+            circular
+            style={{ maxWidth: '960px', minWidth: '280px' }}
+            showItemNavigators
+            item={itemTemplate}
+            title="Obras Terminadas"
+          />
+        )}
+      </div>
+      <div className="container-gallery card flex flex-column justify-content-center align-items-center">
+      <h2 className="titulo-galeria text-center m-0 pt-3">Obras en Construcción</h2>
+        {images2 && (
+          <Galleria
+            className="gallery"
+            value={images2}
+            responsiveOptions={responsiveOptions}
+            numVisible={4}
+            circular
+            style={{ maxWidth: '960px', minWidth: '280px' }}
+            showItemNavigators
+            item={itemTemplate}
+            title="Construcciones"
+          />
+        )}
+      </div>
     </>
   )
 };
